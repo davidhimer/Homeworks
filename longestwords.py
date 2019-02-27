5229db2d50aba4b4971c4195246232a6b327d2e3
davidhimer <47630608+davidhimer@users.noreply.github.com>
2019-02-27 19:29:05 +0100

    Create longestwords.py

---
+++ longestwords.py
@@ -0,0 +1,18 @@
+import re
+
+infile = open("test.txt" , "r")
+outfile = open("Listoflongestwords.txt" , "w")
+
+szavak = re.split(" " , infile)
+hossz = len(szavak[0])
+
+for i in szavak:
+    if(len(i) > hossz):
+        hossz = len(i)
+
+for i in szavak:
+    if(len(i) == hossz):
+        f.write(i , len(i))
+
+infile.close()
+outfile.close()
