eabc0de9b9b1ef47cc6bd4c056c5289e3a1811ae
davidhimer <47630608+davidhimer@users.noreply.github.com>
2019-03-06 20:02:46 +0100

    Update darabszamlalo.py

--- darabszamlalo.py
+++ darabszamlalo.py
@@ -1,9 +1,9 @@
 import re
 
-f=open("test.txt", "r")
+f = open("test.txt", "r")
 str = f.read()
-tomb=[int(s) for s in re.findall(r'-?\d+\.?\d*',str)]
-a =0
+tomb = [int(s) for s in re.findall(r'-?\d+\.?\d*',str)]
+a = 0
 for i in range(len(tomb)):
     a += tomb[i]
 print(a)
